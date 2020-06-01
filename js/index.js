08e91d9b943ed3a01ef0026b3bb4a12c14157b61
Claudia Marguerie <c_guevara02@yahoo.fr>
2020-06-01 16:10:54 +0900

    JS file corrected error

--- js/index.js
+++ js/index.js
@@ -16,14 +16,14 @@ function displayTextEnd(){
     listProduct.appendChild(textBoxEnd);
 }
 
-// function displayProduct(product){
-//     const ul = document.querySelector('#list-products');
-//     const li = document.createElement('li');
-//     li.innerHTML = '<div>' +
-//         '<a href="product.html?id='+ product._id +'">' + product.name + '</a>' +
-//         '</div>'
-//     ul.appendChild(li);
-}
+// // function displayProduct(product){
+// //     const ul = document.querySelector('#list-products');
+// //     const li = document.createElement('li');
+// //     li.innerHTML = '<div>' +
+// //         '<a href="product.html?id='+ product._id +'">' + product.name + '</a>' +
+// //         '</div>'
+// //     ul.appendChild(li);
+// }
 
 ajax.get('http://localhost:3000/api/cameras').then((products) => {
     for(let i = 0; i < products.length; i++){
